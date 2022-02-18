6ea652d38b50f0e1fbd0193f73d3123768ba10da
divineLush <alvipath@yahoo.com>
2022-02-18 11:14:40 -0500

    even more caesar improvements

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -22,29 +22,19 @@ def decrypt(n, txt):
     return result
 
 
-def calc_score(str):
+def score(str):
     e = (str.count("e") + str.count("E")) * 0.13
     a = (str.count("a") + str.count("A")) * 0.82
     i = (str.count("i") + str.count("I")) * 0.7
     o = (str.count("o") + str.count("O")) * 0.75
+    u = (str.count("u") + str.count("U")) * 0.28
+    y = (str.count("y") + str.count("Y")) * 0.2
 
-    return e + a + i + o
-
-
-def compare_results(a, b):
-    a_score = calc_score(a)
-    b_score = calc_score(b)
-
-    if (a_score > b_score):
-        return 1
-    elif (a_score == b_score):
-        return 0
-    else:
-        return -1
+    return e + a + i + o + u + y
 
 
 dec = [decrypt(i, input) for i in range(1, 26)]
-dec_sorted = sorted(dec, key=lambda x: calc_score(x), reverse=True)
+dec_sorted = sorted(dec, key=lambda x: score(x), reverse=True)
 
 for entry in dec_sorted:
     print(entry)
