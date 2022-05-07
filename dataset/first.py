8bab94f1da3f53c802dea01b2afe5571acb0dad7
divineLush <alvipath@yahoo.com>
2022-05-07 06:56:42 -0400

    first

--- dataset/first.py
+++ dataset/first.py
@@ -2,6 +2,20 @@ import csv
 
 with open('data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
-    first_col = [row[0] for row in reader]
-    first_col.pop(0)
-    print(first_col)
+    data = [(row[0], row[1]) for row in reader]
+    data.pop(0)
+    print(data)
+
+    ln = len(data[0][0])
+    res = [0] * ln
+
+    for row in data:
+        for i in range(ln):
+            if row[0][i] == '1':
+                res[i] += int(row[1])
+
+
+    with open('first_patient_output', 'w') as csvoutput:
+        writer = csv.writer(csvoutput)
+        writer.writerow(list(range(1, 21)))
+        writer.writerow(res)
