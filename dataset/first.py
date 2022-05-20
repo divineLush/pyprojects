da9cc441ba74f9b62c850b759ca52ac86eb5d115
divineLush <alvipath@yahoo.com>
2022-05-20 13:38:49 -0400

    normalize output

--- dataset/first.py
+++ dataset/first.py
@@ -4,23 +4,24 @@ with open('data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     data = [(row[0], row[1]) for row in reader]
     data.pop(0)
-    print(data)
+    # print(data)
 
     ln = len(data[0][0])
     res = [0] * ln
 
-    row_count = len(data)
-
+    sum = 0
     for row in data:
+        val = int(row[1])
+        sum += val
+
         for i in range(ln):
             if row[0][i] == '1':
-                res[i] += int(row[1])
-
-        for i in range(ln):
-            res[i] /= row_count
+                res[i] += val
 
 
-    with open('first_patient_output', 'w') as csvoutput:
+    res = [num / sum for num in res]
+
+    with open('first_patient_output.txt', 'w') as csvoutput:
         writer = csv.writer(csvoutput)
         writer.writerow(list(range(1, 21)))
         writer.writerow(res)
